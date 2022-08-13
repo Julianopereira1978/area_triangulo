5f504c89e4fcdf2c8088b77ff5b7fb9f740d3cbc
Juliano Antonio de Abreu Pereira <jubamarvim@yahoo.com.br>
2022-08-13 18:27:36 -0300

    Add files via upload

---
+++ area triangulo/area_triangulo.py
@@ -0,0 +1,17 @@
+# Created by Juliano Pereira on 13/08/2022
+#calculadora area do triangulo
+
+import os
+
+print("Informe os dados do triangulo: ")
+
+base = float(input('Base: '))
+
+altura = float(input('Altura: '))
+
+area = base * altura / 2
+
+print ('A área do triângulo é' , area)
+     
+
+os.system('pause')
